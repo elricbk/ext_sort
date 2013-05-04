b58b9025738c277584284755f68cc313a810a475
Boris Kuchin <elricbk@gmail.com>
2013-05-04 16:07:19 +0400

    autotests;

---
+++ tests/file_comparer.hpp
@@ -0,0 +1,113 @@
+#include <cstdio>
+
+#include <boost/bind.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/foreach.hpp>
+#include <boost/throw_exception.hpp>
+
+#include <log4cpp/Category.hh>
+
+#include "common/record_info.h"
+
+class file_comparer_t
+{
+public:
+  file_comparer_t(const std::string& fname_orig = "original.test.dat", const std::string& fname_sort = "sorted.test.dat")
+    : m_logger(log4cpp::Category::getRoot())
+    , m_fname_orig(fname_orig)
+    , m_fname_sort(fname_sort) {}
+
+  ~file_comparer_t()
+  {
+    if (remove(m_fname_orig.c_str()) != 0)
+      m_logger.errorStream() << "Error while deleting file " << m_fname_orig;
+    if (remove(m_fname_sort.c_str()) != 0)
+      m_logger.errorStream() << "Error while deleting file " << m_fname_sort;
+  }
+
+  const std::string& fname_orig() { return m_fname_orig; }
+
+  const std::string& fname_sort() { return m_fname_sort; }
+
+  bool file_is_sorted()
+  {
+    if (!m_rec_sort)
+      m_rec_sort = records_from_file(m_fname_sort);
+
+    for (size_t i = 0; i < m_rec_sort->size() - 1; ++i) {
+      if (!(m_rec_sort->at(i) < m_rec_sort->at(i + 1))) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  bool records_are_equal()
+  {
+    if (!m_rec_orig) {
+      m_rec_orig = records_from_file(m_fname_orig);
+      std::sort(m_rec_orig->begin(), m_rec_orig->end());
+    }
+    if (!m_rec_sort)
+      m_rec_sort = records_from_file(m_fname_sort);
+
+    m_logger.info("Original file record count: %u", m_rec_orig->size());
+    m_logger.info("Sorted file record count: %u", m_rec_sort->size());
+
+    if (m_rec_orig->empty()) {
+      m_logger.warn("No entries found in original file");
+      return false;
+    }
+
+    if (m_rec_sort->empty()) {
+      m_logger.warn("No entries found in sorted file");
+      return false;
+    }
+
+    if (m_rec_sort->size() != m_rec_orig->size()) {
+      m_logger.warn("Record count in files differ");
+    }
+
+    for (size_t i = 0; i < m_rec_orig->size(); ++i) {
+      if (!keys_are_equal(m_rec_orig->at(i), m_rec_sort->at(i))) {
+        m_logger.warn("Keys differ at index %u", i);
+        return false;
+      }
+    }
+    return true;
+  }
+
+
+private:
+  typedef boost::shared_ptr<std::vector<record_info_t> > record_list_t;
+
+  static record_list_t records_from_file(const std::string& fname)
+  {
+    std::ifstream is(fname.c_str(), std::ifstream::binary);
+    record_list_t result(new std::vector<record_info_t>());
+    while (is.good())
+    {
+      test cur_test;
+      size_t offset = is.tellg();
+      is.read(reinterpret_cast<char*>(&cur_test), sizeof(cur_test));
+      std::streamsize bytes_read = is.gcount();
+      if (bytes_read != sizeof(cur_test))
+        BOOST_THROW_EXCEPTION(std::runtime_error("Unable to read test struct from input file"));
+      is.ignore(cur_test.size);
+      result->push_back(record_info_t(cur_test, offset));
+    }
+    return result;
+  }
+
+  static bool keys_are_equal(const record_info_t& first, const record_info_t& second)
+  {
+    return std::memcmp(first.key, second.key, 64) == 0;
+  }
+
+private:
+  log4cpp::Category& m_logger;
+  std::string m_fname_orig;
+  std::string m_fname_sort;
+  record_list_t m_rec_orig;
+  record_list_t m_rec_sort;
+};
---
+++ tests/test_auto.cpp
@@ -0,0 +1,73 @@
+#define BOOST_TEST_MODULE Autotests
+#define BOOST_TEST_DYN_LINK
+
+#include <boost/test/unit_test.hpp>
+
+#include "log4cpp/Category.hh"
+#include "log4cpp/Appender.hh"
+#include "log4cpp/FileAppender.hh"
+#include "log4cpp/PatternLayout.hh"
+#include "log4cpp/Priority.hh"
+
+#include "sorter/sorter.hpp"
+#include "generator/generator.hpp"
+#include "file_comparer.hpp"
+
+struct LoggingSetup
+{
+  LoggingSetup()
+  {
+    log4cpp::PatternLayout* layout = new log4cpp::PatternLayout();
+    layout->setConversionPattern("%d [%p] %m%n");
+    log4cpp::Appender *appender = new log4cpp::FileAppender("default", "test_auto.log");
+    appender->setLayout(layout);
+
+    log4cpp::Category& root = log4cpp::Category::getRoot();
+    root.setPriority(log4cpp::Priority::INFO);
+    root.addAppender(appender);
+  }
+};
+
+BOOST_GLOBAL_FIXTURE(LoggingSetup)
+
+BOOST_AUTO_TEST_SUITE(SimpleScnearios)
+
+  BOOST_AUTO_TEST_CASE(single_temp_file) {
+    file_comparer_t comparer;
+
+    generator_t generator(comparer.fname_orig(), 1024, 50);
+    generator.generate_data();
+
+    sorter_t sorter(comparer.fname_orig(), comparer.fname_sort(), 20480);
+    sorter.sort_data();
+
+    BOOST_CHECK(comparer.file_is_sorted());
+    BOOST_CHECK(comparer.records_are_equal());
+  }
+
+  BOOST_AUTO_TEST_CASE(multiple_temp_files) {
+    file_comparer_t comparer;
+
+    generator_t generator(comparer.fname_orig(), 20480, 50);
+    generator.generate_data();
+
+    sorter_t sorter(comparer.fname_orig(), comparer.fname_sort(), 1024);
+    sorter.sort_data();
+
+    BOOST_CHECK(comparer.file_is_sorted());
+    BOOST_CHECK(comparer.records_are_equal());
+  }
+
+  BOOST_AUTO_TEST_CASE(sparse_file) {
+    file_comparer_t comparer;
+
+    generator_t generator(comparer.fname_orig(), 16*1024*1024, 4*1024*1024);
+    generator.generate_data();
+
+    sorter_t sorter(comparer.fname_orig(), comparer.fname_sort(), 8*1024*1024);
+    sorter.sort_data();
+
+    BOOST_CHECK(comparer.file_is_sorted());
+    BOOST_CHECK(comparer.records_are_equal());
+  }
+BOOST_AUTO_TEST_SUITE_END()
