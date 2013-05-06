4295bfb9575b4807ee60be8839cd97ac3989a59e
Boris Kuchin <elricbk@gmail.com>
2013-05-06 18:23:15 +0400

    refactored input_file_t to separate file;

--- sorter/input_buffer.h
+++ sorter/input_buffer.h
@@ -1,19 +1,13 @@
 #pragma once
 
-#include <cstdio>
-#include <fstream>
 #include <istream>
 
 #include <boost/shared_array.hpp>
 #include <boost/throw_exception.hpp>
-#include <boost/iostreams/device/file.hpp>
-#include <boost/iostreams/stream.hpp>
 
 #include "log4cpp/Category.hh"
 
 #include "common/record.h"
-
-namespace io = boost::iostreams;
 
 // FIXME: explicit constructors where possible?
 
@@ -136,34 +130,4 @@ private:
   boost::shared_array<char> m_data;
   size_t m_idx;
   size_t m_data_available;
-};
-
-// Обёртка над входным файлом, нужна чтобы буфер было проще тестировать
-class input_file_t {
-public:
-  input_file_t (const std::string& fname, size_t ram_size, bool rm_file = false)
-    : m_logger(log4cpp::Category::getRoot())
-    , m_delete_file(rm_file)
-    , m_file_name(fname)
-    , m_sbuf(m_file_name)
-    , m_buffer(m_sbuf, ram_size) {}
-
-  ~input_file_t()
-  {
-    m_sbuf.close();
-    if (m_delete_file) {
-      m_logger.debugStream() << "Removing file " << m_file_name;
-      if (remove(m_file_name.c_str()) != 0)
-        m_logger.errorStream() << "Error while deleting file " << m_file_name; 
-    }
-  }
-
-  input_buffer_t& buffer() { return m_buffer; }
-
-private:
-  log4cpp::Category& m_logger;
-  bool m_delete_file;
-  std::string m_file_name;
-  io::stream<io::file_source> m_sbuf;
-  input_buffer_t m_buffer;
 };
---
+++ sorter/input_file.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <cstdio>
+
+#include <boost/iostreams/device/file.hpp>
+#include <boost/iostreams/stream.hpp>
+
+#include "log4cpp/Category.hh"
+
+#include "input_buffer.h"
+
+namespace io = boost::iostreams;
+
+// Обёртка над входным файлом, чтобы буфер можно было тестировать отдельно
+class input_file_t {
+public:
+  input_file_t (const std::string& fname, size_t ram_size, bool rm_file = false)
+    : m_logger(log4cpp::Category::getRoot())
+    , m_delete_file(rm_file)
+    , m_file_name(fname)
+    , m_sbuf(m_file_name)
+    , m_buffer(m_sbuf, ram_size) {}
+
+  ~input_file_t()
+  {
+    m_sbuf.close();
+    if (m_delete_file) {
+      m_logger.debugStream() << "Removing file " << m_file_name;
+      if (remove(m_file_name.c_str()) != 0)
+        m_logger.errorStream() << "Error while deleting file " << m_file_name; 
+    }
+  }
+
+  input_buffer_t& buffer() { return m_buffer; }
+
+private:
+  log4cpp::Category& m_logger;
+  bool m_delete_file;
+  std::string m_file_name;
+  io::stream<io::file_source> m_sbuf;
+  input_buffer_t m_buffer;
+};
